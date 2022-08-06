db919af1cfa43009d6f1cd6d5831b6110c33c6e5
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2022-08-06 20:25:49 +1000

    New stuff

---
+++ kubejs/server_scripts/script.js
@@ -0,0 +1,8 @@
+// priority: 0
+
+settings.logAddedRecipes = false
+settings.logRemovedRecipes = true
+settings.logSkippedRecipes = true
+settings.logErroringRecipes = true
+
+console.info('All The Blocks Code Compat is initialising!')

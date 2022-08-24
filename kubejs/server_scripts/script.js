0c260366d3eedd0cdef4109bd3a0d1245f201897
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2022-08-24 22:44:29 +1000

    Delete kubejs directory

--- kubejs/server_scripts/script.js
+++
@@ -1,8 +0,0 @@
-// priority: 0
-
-settings.logAddedRecipes = false
-settings.logRemovedRecipes = true
-settings.logSkippedRecipes = true
-settings.logErroringRecipes = true
-
-console.info('All The Blocks Code Compat is initialising!')

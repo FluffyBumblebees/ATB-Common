010cbf1affdf11ac0de0420c54ac6f290b33bf2c
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2022-08-25 23:16:03 +1000

    add custom shader code
    This will be built upon!

---
+++ shaders/waving.glsl
@@ -0,0 +1,4 @@
+	#ifdef WAVING_LEAVES
+    if (mc_Entity.x == 12363) // Neon Equisetum
+        wave += calcMove(worldpos, 0.0040, 0.0064, 0.0043, 0.0035, 0.0037, 0.0041, vec3(0.9, 0.6, 0.8), vec3(0.45,0.3,0.4));
+    #endif

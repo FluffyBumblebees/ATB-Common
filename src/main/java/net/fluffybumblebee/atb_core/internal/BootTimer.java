09e99f932c1d8d0a5462619cc4dd2b767c7100ab
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2023-02-08 00:40:46 +1100

    finally working with AOF

--- src/main/java/net/fluffybumblebee/atb_core/internal/BootTimer.java
+++ src/main/java/net/fluffybumblebee/atb_core/internal/BootTimer.java
@@ -6,13 +6,9 @@ import org.apache.commons.lang3.time.StopWatch;
 import java.util.concurrent.TimeUnit;
 
 public class BootTimer {
-    public static long BOOTSTRAP_TIME_CACHE = 0;
     public static final StopWatch STOP_WATCH = new SafeStopWatch();
 
     public static long getTimeInSeconds() {
         return STOP_WATCH.getTime(TimeUnit.SECONDS);
-    }
-    public static long getTimeInSecondsSinceBoot() {
-        return getTimeInSeconds() - BOOTSTRAP_TIME_CACHE;
     }
 }
--- src/main/java/net/fluffybumblebee/atb_core/internal/mixin/SpriteAtlasHolderMixin.java
+++ src/main/java/net/fluffybumblebee/atb_core/internal/mixin/SpriteAtlasHolderMixin.java
@@ -3,6 +3,7 @@ package net.fluffybumblebee.atb_core.internal.mixin;
 import net.fluffybumblebee.atb_core.core.ATBCore;
 import net.minecraft.client.texture.SpriteAtlasHolder;
 import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.client.texture.StatusEffectSpriteManager;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.profiler.Profiler;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,22 +11,22 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static net.fluffybumblebee.atb_core.internal.BootTimer.*;
+import static net.fluffybumblebee.atb_core.internal.BootTimer.STOP_WATCH;
+import static net.fluffybumblebee.atb_core.internal.BootTimer.getTimeInSeconds;
 
 @Mixin(SpriteAtlasHolder.class)
-public class SpriteAtlasHolderMixin {
-    private static boolean FIRST = false;
+public abstract class SpriteAtlasHolderMixin {
     private static boolean EXPIRED = false;
 
+    @SuppressWarnings("ConstantValue")
     @Inject(method = "apply(Lnet/minecraft/client/texture/SpriteAtlasTexture$Data;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At("TAIL"))
     private void timerEnd(SpriteAtlasTexture.Data data, ResourceManager resourceManager, Profiler profiler, CallbackInfo ci) {
-        if (FIRST && !EXPIRED) {
+        if (!EXPIRED && (SpriteAtlasHolder)(Object)this instanceof StatusEffectSpriteManager) {
             STOP_WATCH.stop();
             ATBCore.LOGGER.info("");
             ATBCore.LOGGER.info("Loading time (Seconds): " + getTimeInSeconds());
-            ATBCore.LOGGER.info("Loading time since bootstrap (Seconds): " + getTimeInSecondsSinceBoot());
             ATBCore.LOGGER.info("");
             EXPIRED = true;
-        } else FIRST = true;
+        }
     }
 }
